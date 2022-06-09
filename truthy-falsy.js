4dfa7a49ce9facc5779b54fcd56b5b7782d1ea4b
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-06-09 08:31:43 -0500

    first commit

---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+// Truthy y Falsy 
+
+// Son valores que son verdaderos o falsos 
+
+// Falsy (False)
+
+"" // un string vacío 
+0 -0
+null
+NaN
+false
+undefined
+
+//Truthy (true)
+
+"hola"
+30
+{a:1}
+[1,3]
+true
+function a(){}
+más 
+
